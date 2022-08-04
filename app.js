d68c76ba13419934d0b8f769de0f5d3e8b1e17c3
abdelrahman <gadoua50@gmail.com>
2022-08-04 17:41:06 +0200

    set views directory and view engine

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const MongoStore = require("connect-mongo");
 const bycrpt = require("bcryptjs");
+const path = require("path");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT;
+const DB_STRING = process.env.DB_STRING;
 
 const app = express();
+
+// Set View
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(PORT);
