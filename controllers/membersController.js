eff71eb1e020d559ed216ae80ce0a9d6d7b701f0
abdelrahman <gadoua50@gmail.com>
2022-08-04 23:08:15 +0200

    add initial controllers

---
+++ controllers/membersController.js
@@ -0,0 +1,39 @@
+exports.sign_up_get = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.sign_up_post = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.log_in_get = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.log_in_post = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.log_out_get = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.clubhouse_get = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.clubhouse_post = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.admin_get = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.admin_post = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
+
+exports.about_get = (req, res, next) => {
+  res.send("Not Implemented YET");
+};
