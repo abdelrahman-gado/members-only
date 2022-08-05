40d93526364231b8113a6d5607104aacff4d390b
abdelrahman <gadoua50@gmail.com>
2022-08-05 16:56:33 +0200

    implement get controllers

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -19,7 +19,7 @@ exports.log_out_get = (req, res, next) => {
 };
 
 exports.clubhouse_get = (req, res, next) => {
-  res.send("Not Implemented YET");
+  res.render("clubhouse-form")
 };
 
 exports.clubhouse_post = (req, res, next) => {
@@ -27,7 +27,7 @@ exports.clubhouse_post = (req, res, next) => {
 };
 
 exports.admin_get = (req, res, next) => {
-  res.send("Not Implemented YET");
+  res.render("admin-form");
 };
 
 exports.admin_post = (req, res, next) => {
@@ -35,7 +35,7 @@ exports.admin_post = (req, res, next) => {
 };
 
 exports.add_message_get = (req, res, next) => {
-  res.send("Not Implemneted YET");
+  res.render("add-message");
 };
 
 exports.add_message_post = (req, res, next) => {
@@ -43,7 +43,7 @@ exports.add_message_post = (req, res, next) => {
 };
 
 exports.about_get = (req, res, next) => {
-  res.send("Not Implemented YET");
+  res.render("about.ejs");
 };
 
 exports.delete_message_get = (req, res, next) => {
